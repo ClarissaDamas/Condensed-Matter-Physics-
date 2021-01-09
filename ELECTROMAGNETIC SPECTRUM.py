eaa4fefce9f22193bf5a74ffe3a416d235f8bbd5
Clarissa <physicscla@gmail.com>
2021-01-09 16:39:10 -0300

    New file about Helium Neon laser

---
+++ ELECTROMAGNETIC SPECTRUM.py
@@ -0,0 +1,40 @@
+class electromagnetic_waves:
+
+    def __init__(self,wave_length_range,production,properties):
+
+        self.wave_length_range = wave_length_range
+        self.production = production
+        self.properties = properties
+
+    def add_new_wave(self,)
+
+class Gamma_Rays(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+
+class X_rays(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+class Ultra_violet_Radiation(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+class Visible_Radiation(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+class Infra_red_Radiation(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+class Hertzian_Waves(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+class Radio_Waves(electromagnetic_waves):
+    def __init__(self,wave_length_range,production,properties):
+        super().__init__(kskkds)
+
+wave = input('Are you ready to do the quiz about waves?(please,answer yes or no,only).')
